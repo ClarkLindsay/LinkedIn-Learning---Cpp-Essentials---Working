c6e289e3cec953426442a02da311464376fd4708
ClarkLindsay <clark.lindsay@siu.edu>
2022-09-15 12:43:32 -0400

    Initial Commit

---
+++ Working/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Working
+//
+//  Created by Clark Lindsay on 9/15/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
